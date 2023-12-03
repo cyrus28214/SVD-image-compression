db3235ced4c040e2a715d391bc683348422ed057
Cyrus28214 <1270302477@qq.com>
2023-12-04 02:05:57 +0800

    add CLI & modify README.md

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 使用奇异值分解（SVD）实现了图像压缩算法。这是学习线性代数时给自己布置的作业
+
+```
+usage: svd.py [-h] [-c FILE] [-o FILE] [-r RATE] [-d FILE] [-p FILE]
+              [--rates RATES [RATES ...]]
+
+SVD Image Compression
+
+options:
+  -h, --help            show this help message and exit
+  -c FILE, --compress FILE
+                        Compress an image
+  -o FILE, --output FILE
+                        Specify output file for compression
+  -r RATE, --rate RATE  Compression rate
+  -d FILE, --decompress FILE
+                        Decompress a compressed file
+  -p FILE, --preview FILE
+                        Preview the compressed images
+  --rates RATES [RATES ...]
+                        Specify compression rates for preview
+```
--- svd.py
+++ svd.py
@@ -2,10 +2,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 import PIL.Image
 import os
+import argparse
+
+def load_img(path):
+    img = PIL.Image.open(path)
+    img = np.array(img).astype('float32')
+    return img
 
 def compress(path, rate):
-    img = PIL.Image.open(path)
+    img = load_img(path)
+    img = np.transpose(img, (2, 0, 1)) # (m, n, 3) -> (3, m, n)
+    u, s, v = np.linalg.svd(img) # SVD分解
+    if rate >= 1:
+        return u, s, v
     m, n = img.size
+    k = rate_to_k(m, n, rate)
+    return preserve_k(u, s, v, k)
+
+def preserve_k(u, s, v, k):
+    u = u[..., :k] # 保留前k列
+    s = s[:, :k] # 保留前k个奇异值
+    v = v[:, :k] # 保留前k行
+    return u, s, v    
+
+def rate_to_k(m, n, rate):
     '''
     设原图像size为m*n
     则占用空间为m*n*3
@@ -14,14 +34,8 @@ def compress(path, rate):
     压缩率为rate=(m+n+1)*k*4/(m*n)
     k = rate*m*n/((m+n+1)*4)
     '''
-    k = int(rate*m*n/((m+n+1)*4))
-    img = np.array(img).astype('float32')
-    img = np.transpose(img, (2, 0, 1)) # (m, n, 3) -> (3, m, n)
-    u, s, v = np.linalg.svd(img) # SVD分解
-    u = u[..., :k] # 保留前k列
-    s = s[:, :k] # 保留前k个奇异值
-    v = v[:, :k] # 保留前k行
-    return u, s, v
+    return int(rate*m*n/((m+n+1)*4))
+
 
 def decompress(u, s, v):
     img = (u * s[:, np.newaxis]) @ v # (3, m, k) * (3, 1, k) @ (3, k, n) -> (3, m, n)
@@ -29,25 +43,71 @@ def decompress(u, s, v):
     img = np.round(img.clip(0, 255)).astype('uint8')
     return img
 
-def preview(path, rates = [
-        1, 0.95, 0.9, 0.8, 0.7, 
-        0.6, 0.5, 0.4, 0.3, 0.2
-    ], col=5):
+def preview(path, rates, col=5):
     row = (len(rates) + col - 1) // col
     fig, axes = plt.subplots(row, col)
+    for i in axes.flat:
+        i.axis('off')
+    u, s, v = compress(path, 1)
+    m, n = PIL.Image.open(path).size
     for i, rate in enumerate(rates):
-        img = decompress(*compress(path, rate))
+        k = rate_to_k(m, n, rate)
+        img = decompress(*preserve_k(u, s, v, k))
         ax = axes[i // col, i % col]
         ax.set_title(f'rate={rate}')
         ax.imshow(img)
-        ax.axis('off')
     plt.show()
 
 def save(path, u, s, v):
-    np.savez(path, u=u, s=s, v=v)
+    np.savez_compressed(path, u=u, s=s, v=v)
 
-def load(path):
+def load_c(path):
     d = np.load(path)
     return d['u'], d['s'], d['v']
 
-preview('Lenna.png')
+def main():
+    parser = argparse.ArgumentParser(description="SVD Image Compression")
+    
+    # Compression options
+    parser.add_argument('-c', '--compress', metavar='FILE', help='Compress an image')
+    parser.add_argument('-o', '--output', metavar='FILE', help='Specify output file for compression')
+    parser.add_argument('-r', '--rate', type=float, help='Compression rate')
+
+    # Decompression options
+    parser.add_argument('-d', '--decompress', metavar='FILE', help='Decompress a compressed file')
+
+    # Preview options
+    parser.add_argument('-p', '--preview', metavar='FILE', help='Preview the compressed images')
+    parser.add_argument('--rates', type=float, nargs='+', help='Specify compression rates for preview')
+
+    args = parser.parse_args()
+
+    if args.compress:
+        u, s, v = compress(args.compress, args.rate or 0.8)
+        output = args.output or args.compress
+        if not output.endswith('.npz'):
+            output += '.npz'
+        save(output, u, s, v)
+        print(f'Image compressed and saved to {output}')
+
+    elif args.decompress:
+        u, s, v = load_c(args.decompress)
+        output = args.output or args.decompress
+        if output.endswith('.npz'):
+            output = output[:-4]
+        img = decompress(u, s, v)
+        PIL.Image.fromarray(img).save(output)
+        print(f'Image decompressed and saved to {output}')
+
+    elif args.preview:
+        rates = args.rates or [
+            1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2
+        ]
+        preview(args.preview, rates)
+
+    else:
+        parser.print_help()
+
+if __name__ == '__main__':
+    main()
+
